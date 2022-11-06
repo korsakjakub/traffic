87b55ac4edacf967a212ade7c51301ab76350ddf
Jakub Korsak <jakub.korsak@protonmail.com>
2022-11-06 16:17:15 +0100

    png dir to config

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 class Config:
     road_size = 100
-    noise = 0.1  # How many percent of cars brake each turn
+    noise = 0.2  # How many percent of cars brake each turn
     cars_amount = 30
     time_range = 40
+    png_dir = 'png/'
 

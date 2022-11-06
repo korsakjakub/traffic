287f5f58332ebcc9e90291efd2ad743bb0fe756f
Jakub Korsak <jakub.korsak@protonmail.com>
2022-11-06 14:44:01 +0100

    moving is not respecting pbc's

--- action.py
+++
@@ -1,7 +0,0 @@
-from enum import Enum
-
-
-class Action(Enum):
-    ACCELERATE = 1
-    BRAKE = 2
-    RANDOM = 3
--- car.py
+++ car.py
@@ -1,25 +1,30 @@
-from action import Action
+from config import Config
 
 
 class Car:
-    def __init__(self, position: int, velocity: int = 0, action: Action = None):
+    def __init__(self, position: int, velocity: int = 0, params=None):
+        if params is None:
+            params = {}
         self.position = position
         self.velocity = velocity
-        self.action = action
+        self.road_size = params["road_size"] if "road_size" in params else None
 
-    def accelerate(self):
+    def accelerate(self, road_size):
         self.velocity += 1
-        self._move()
+        self._move(road_size)
         return self
 
     def brake(self):
-        self.velocity -= 1
-        self._move()
-        return self
-
-    def random_slowdown(self):
         self.velocity = 0
         return self
 
-    def _move(self):
+    def random_slowdown(self):
+        self.brake()
+        return self
+
+    def _move(self, rs):
         self.position += self.velocity
+        if self.position >= rs:
+            self.position -= rs
+        elif self.position < 0:
+            self.position += rs
